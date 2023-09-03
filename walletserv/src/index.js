fa038bbe76c8caf2f75184d530da75de2174b3d4
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2023-09-04 01:04:38 +0800

    Update wallet creation endpoint

--- walletserv/src/index.js
+++ walletserv/src/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const walletObjects = [{
   "user_id": 123456,          // Numeric: User ID
@@ -12,20 +13,25 @@ const walletObjects = [{
   "transaction_limit": 500   // Numeric: Transaction Limit
 }];
 
-// CRUD
 app.post("/wallet", (req, res) => {
-
-  const {user_id, currency, transaction_limit} = req.body;
+  const { user_id, currency, transaction_limit } = req.body;
 
   if (!user_id || !currency || !transaction_limit) {
-    return res.status(400).json({error: "Missing required fields"});
+    return res.status(400).json({ error: "Missing required fields" });
   }
 
-  const newWallet = {user_id: user_id, balance: 0, currency: currency, loyalty_points: 0, transaction_limit: transaction_limit};
+  const newWallet = {
+    user_id,
+    balance: 0,
+    currency,
+    loyalty_points: 0,
+    transaction_limit,
+  };
 
-  walletObjects.concat(newWallet);
+  walletObjects.push(newWallet);
+
   res.status(201).json(newWallet);
-})
+});
 
 app.listen(3000, () =>
   console.log('Wallet backend listening on port 3000'),
