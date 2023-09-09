49a8645d3d2c324446f259c51fd8b4a8155c1530
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2023-09-09 15:02:54 +0800

    Udpate createWallet express Route

--- walletserv/src/routes/walletRoutes.js
+++ walletserv/src/routes/walletRoutes.js
@@ -1,21 +1,18 @@
 import express from 'express';
-import {
-    createWallet,
-    getWalletByUserId
-} from '../controllers/walletController.js';
+import createWallet from '../controllers/walletController.js';
 
 const router = express.Router();
 
 // Create a new wallet
 router.post('/', createWallet);
 
-// Get wallet by user id
-router.get('/:id', getWalletByUserId);
+// // Get wallet by user id
+// router.get('/:id', getWalletByUserId);
 
-// Update wallet by wallet id
-router.put('/:wallet_id', updateWallet);
+// // Update wallet by wallet id
+// router.put('/:wallet_id', updateWallet);
 
-// Delete a transaction by wallet id
-router.delete('/:wallet_id', deleteTransaction);
+// // Delete a transaction by wallet id
+// router.delete('/:wallet_id', deleteTransaction);
 
 export default router;
