d582c56fbfa876185f6a44415f1fa30f41a55c53
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2023-09-09 15:03:22 +0800

    Update wallet controller create wallet route

---
+++ walletserv/src/controllers/walletController.js
@@ -0,0 +1,40 @@
+import WalletModel from "../models/WalletModel.js";
+
+const createWallet = async (req, res) => {
+    const { user_id, currency, transaction_limit } = req.body;
+
+    if (!user_id || !currency || !transaction_limit) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const wallets = await WalletModel.findAll({
+        where: { user_id },
+    });
+
+    try {
+      // Check if a wallet with the same user_id already exists
+      const existingWallet = await WalletModel.findOne({
+        where: { user_id },
+      });
+  
+      if (existingWallet) {
+        return res.status(400).json({ error: "Wallet already exists for this user" });
+      }
+  
+      // Create a new wallet
+      const newWallet = await WalletModel.create({
+        user_id,
+        balance: 0,
+        currency_code: currency,
+        transaction_limit,
+      });
+  
+      return res.status(201).json(newWallet);
+
+    } catch (error) {
+      console.error("Error creating wallet:", error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+export default createWallet;
